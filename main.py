5a4492d59124fc1e6c5753b6073f7f07867ba4b8
tidesmile <tidesmile@gmail.com>
2025-03-25 09:58:23 +0700

    Add our module to FastAPI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 
-#from program3 import add2,mult2
+from program3 import add2,mult2
 
 app = FastAPI()
 
@@ -17,13 +17,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-#@app.get("/add2/{arg}")
-#def add2_handeler(arg:int):
-#    return {"arg":arg,"add2":add2(arg)}
+@app.get("/add2/{arg}")
+def add2_handeler(arg:int):
+    return {"arg":arg,"add2":add2(arg)}
 
-#@app.get("/mult2/{agr}")
-#def mult2_handler(arg:int):
-#    """
-#    docstring
-#    """
-#    return {"arg":arg,"mult2":mult2(arg)}
+@app.get("/mult2/{agr}")
+def mult2_handler(arg:int):
+    """
+    docstring
+    """
+    return {"arg":arg,"mult2":mult2(arg)}
