4e7a6c28c134fac2118a6e70339a9bda43ed9ce4
tidesmile <tidesmile@gmail.com>
2025-03-25 09:54:33 +0700

    Add modules

---
+++ program3.py
@@ -0,0 +1,9 @@
+def add2(x):
+    return x+2
+
+def mult2(x):
+    return x*2
+
+if __name__ == '__main__':
+    print('add2(1) =', add2(1))
+    print('mult2(1) =', mult2(1))
---
+++ program4.py
@@ -0,0 +1,8 @@
+import program3
+from program3 import add2,mult2
+
+print('Inside program4')
+
+print('add2(3) =', program3.add2(3)) #เรียกใช้งานตัวแปลจาก program3
+print('add2(5) =', add2(5))
+print('mult2(7) =', mult2(7))
